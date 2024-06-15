b8b8b7eac8d05b870958433e4e77291ca0802270
Olegtikh <olegtik@bk.ru>
2024-06-15 23:23:08 +0300

    Add files via upload

---
+++ bot-example-php-master/www/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '6793eb5b'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', 'vk1.a.VxuCuDqvCStPd06pK92274tZYwC6vOctWZJSoZTbCPGSkLWE7q8b57Bx-AOR2wUTXBWD1WxgAf5HiGamy4nctrnyBq-C6aW__jk2xOCX8XjuQNMA0857ZBB9cBVmND5REkuHu5NVh0edXX0NGT4kqawxrsL2QMG7r75WpLz-tHVaVzmmRIYOzNB20ALGtc2j3Q5UYVjD_aeNyZd5TNn96Q'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
